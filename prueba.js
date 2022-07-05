c01cd5be7e163da3126141c4fc887acdf3baa9f3
char <53872669+charlitos3000@users.noreply.github.com>
2022-07-05 10:09:03 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,17 @@
+//Carlos Sanchez
+
+//values
+const arr = [1, 2, 3, 4, 5];
+const toSum = 3;
+const initial = 0;
+
+//arrow functions
+const auxArr = arr.map(element => element + toSum);
+const auxReduct = arr.reduce(
+	(prevVal, currVal) => prevVal + currVal,
+  initial
+);
+
+//display
+console.log("Map: " + auxArr);
+console.log("Reduce: " + auxReduct);
